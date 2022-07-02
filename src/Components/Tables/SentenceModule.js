19ca078aa2ff81a23b557472689bee7971018fa0
David Fertitta <Davidj.fertitta@gmail.com>
2022-07-02 17:14:11 -0500

    left column first table

--- src/Components/Tables/SentenceModule.js
+++ src/Components/Tables/SentenceModule.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import TableBanner from "../TableElements/TableBanner";
+import InteractiveGrammarElements from "../TableElements/InteractiveGrammarElements";
 import { TheSentence } from "../../Assets/Data/TheSentence";
 import "../../Styles/Table.css";
 
@@ -13,6 +14,9 @@ const SentenceModule = () => {
   return (
     <div className="table-container">
       <TableBanner data={data} />
+      <section className="two-columns">
+        <InteractiveGrammarElements data={data} />
+      </section>
     </div>
   );
 };
