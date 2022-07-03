c0bdef0d47a4fae7a3a6547ebe44bdaf5f9732a1
David Fertitta <Davidj.fertitta@gmail.com>
2022-07-03 08:26:12 -0500

    right level up tutorials

--- src/Components/TableElements/LevelUpTutorials.js
+++ src/Components/TableElements/LevelUpTutorials.js
@@ -2,11 +2,29 @@ import React, { useState, useEffect } from "react";
 import "../../Styles/Table.css";
 
 const LevelUpTutorials = ({ data }) => {
-    const [tutorials, setTutorials] = useState([]);
-    
+  const [tutorials, setTutorials] = useState([]);
+  const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    const allTutorials = data.map((tutorial) => tutorial.LEVEL_UP_TUTORIALS);
+    setTutorials(allTutorials);
+
+    const allLinks = data.map((link) => link.LEVEL_UP_TUTORIALS_LINKS);
+    setLinks(allLinks);
+  }, [data]);
+
+  const makeList = () => {
+    return tutorials.map((tutorial, index) => (
+      <a key={index} href={links[index]} className="link">
+        <p className="column-item right">{tutorial}</p>
+      </a>
+    ));
+  };
+
   return (
     <section className="subsection">
       <h4 className="subsection-header">Level Up Tutorials</h4>
+      {makeList()}
     </section>
   );
 };
