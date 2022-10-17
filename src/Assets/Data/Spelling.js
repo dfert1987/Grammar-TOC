2da7240e4a7343906f166f1ae80412df84e775e0
David Fertitta <Davidj.fertitta@gmail.com>
2022-10-17 15:11:28 -0600

    blurb update

--- src/Assets/Data/Spelling.js
+++ src/Assets/Data/Spelling.js
@@ -1,67 +1,78 @@
 export const Spelling = [
-  {
-    MODULE: "Spelling",
-    LESSONS: "Spelling: Opener",
-    LESSONS_LINKS:
-      "https://www.hmhco.com/content/literature/grammar_studio/g6/student/epub/hmh_nglit_na_06_gse_m14/#cards--6le_03_gs_spelling_mo/",
-    GRAMMAR_ASSESSMENTS: "Module Pretest: Spelling",
-    ASSEMENT_ICON: true,
-    EDITABLE_GRAMMAR_PRACTICE: "The Dictionary",
-    EDITABLE_ICON_LINKS:
-      "https://www.hmhco.com/content/literature/into_lit/g6/student/doc/06le_03_pgp_dictionary.docx",
-    LEVEL_UP_TUTORIALS: "Spelling",
-    LEVEL_UP_TUTORIALS_LINKS:
-      "https://www.hmhco.com/content/literature/level_up/lit135/index.html",
-    LEVEL_UP_PRACTICE_TESTS: "Spelling",
-    LEVEL_UP_PRACTICE_TEST_ICONs: true,
-  },
-  {
-    LESSONS: "The Dictionary",
-    LESSONS_LINKS:
-      "https://www.hmhco.com/content/literature/grammar_studio/g6/student/epub/hmh_nglit_na_06_gse_m14/#cards--6le_03_gs_spelling_ml1/",
-    GRAMMAR_ASSESSMENTS: "Module Summative Test: Spelling",
-    ASSEMENT_ICON: true,
-    EDITABLE_GRAMMAR_PRACTICE: "Spelling Rules",
-    EDITABLE_ICON_LINKS:
-      "https://www.hmhco.com/content/literature/into_lit/g6/student/doc/06le_03_pgp_spellingrules.docx",
-    LEVEL_UP_TUTORIALS: "Spelling Plural Nouns",
-    LEVEL_UP_TUTORIALS_LINKS:
-      "https://www.hmhco.com/content/literature/level_up/lit136/index.html",
-    LEVEL_UP_PRACTICE_TESTS: "Spelling Plural Nouns",
-    LEVEL_UP_PRACTICE_TEST_ICONs: true,
-  },
-  {
-    LESSONS: "Spelling Rules",
-    LESSONS_LINKS:
-      "https://www.hmhco.com/content/literature/grammar_studio/g6/student/epub/hmh_nglit_na_06_gse_m14/#cards--6le_03_gs_spelling_ml2/",
-    EDITABLE_GRAMMAR_PRACTICE: "Prefixes and Suffixes",
-    EDITABLE_ICON_LINKS:
-      "https://www.hmhco.com/content/literature/into_lit/g6/student/doc/06le_03_pgp_prefixessuffixes.docx",
-    LEVEL_UP_TUTORIALS: "Spelling Words Often Confused",
-    LEVEL_UP_TUTORIALS_LINKS:
-      "https://www.hmhco.com/content/literature/level_up/lit137/index.html",
-    LEVEL_UP_PRACTICE_TESTS: "Spelling Words Often Confused",
-    LEVEL_UP_PRACTICE_TEST_ICONs: true,
-  },
-  {
-    LESSONS: "Prefixes and Suffixes",
-    LESSONS_LINKS:
-      "https://www.hmhco.com/content/literature/grammar_studio/g6/student/epub/hmh_nglit_na_06_gse_m14/#cards--6le_03_gs_spelling_ml3/",
-    EDITABLE_GRAMMAR_PRACTICE: "Plurals of Nouns",
-    EDITABLE_ICON_LINKS:
-      "https://www.hmhco.com/content/literature/into_lit/g6/student/doc/06le_03_pgp_pluralsnouns.docx",
-  },
-  {
-    LESSONS: "Plurals of Nouns",
-    LESSONS_LINKS:
-      "https://www.hmhco.com/content/literature/grammar_studio/g6/student/epub/hmh_nglit_na_06_gse_m14/#cards--6le_03_gs_spelling_ml4/",
-    EDITABLE_GRAMMAR_PRACTICE: "Spelling Module Review",
-    EDITABLE_ICON_LINKS:
-      "https://www.hmhco.com/content/literature/into_lit/g6/student/doc/06le_03_pgp_spellingmodrev.docx",
-  },
-  {
-    EDITABLE_GRAMMAR_PRACTICE: "Grade 6 Printable Grammar Practice Answer Key",
-    EDITABLE_ICON_LINKS:
-      "https://www.hmhco.com/content/literature/into_lit/g6/teacher/pdf/06le_03_pgp_answerkey.pdf",
-  },
+    {
+        MODULE: 'Spelling',
+        LESSONS: 'Spelling: Opener',
+        LESSONS_LINKS:
+            'https://www.hmhco.com/content/literature/grammar_studio/g8/student/epub/hmh_nglit_na_08_gse_m16/#cards--8le_03_gs_spelling_mo/',
+        GRAMMAR_ASSESSMENTS: 'Module Pretest: Spelling',
+        ASSEMENT_ICON: true,
+        EDITABLE_GRAMMAR_PRACTICE: 'The Dictionary',
+        EDITABLE_ICON_LINKS:
+            'https://www.hmhco.com/content/literature/into_lit/g8/student/doc/08le_03_pgp_dictionary.docx',
+        LEVEL_UP_TUTORIALS: 'Spelling',
+        LEVEL_UP_TUTORIALS_LINKS:
+            'https://www.hmhco.com/content/literature/level_up/lit135/index.html',
+        LEVEL_UP_PRACTICE_TESTS: 'Spelling',
+        LEVEL_UP_PRACTICE_TEST_ICONs: true,
+    },
+    {
+        LESSONS: 'The Dictionary',
+        LESSONS_LINKS:
+            'https://www.hmhco.com/content/literature/grammar_studio/g8/student/epub/hmh_nglit_na_08_gse_m16/#cards--8le_03_gs_spelling_ml1/',
+        GRAMMAR_ASSESSMENTS: 'Module Summative Test: Spelling',
+        ASSEMENT_ICON: true,
+        EDITABLE_GRAMMAR_PRACTICE: 'Spelling Rules',
+        EDITABLE_ICON_LINKS:
+            'https://www.hmhco.com/content/literature/into_lit/g8/student/doc/08le_03_pgp_spellingrules.docx',
+        LEVEL_UP_TUTORIALS: 'Spelling Plural Nouns',
+        LEVEL_UP_TUTORIALS_LINKS:
+            'https://www.hmhco.com/content/literature/level_up/lit136/index.html',
+        LEVEL_UP_PRACTICE_TESTS: 'Spelling Plural Nouns',
+        LEVEL_UP_PRACTICE_TEST_ICONs: true,
+    },
+    {
+        LESSONS: 'Spelling Rules',
+        LESSONS_LINKS:
+            'https://www.hmhco.com/content/literature/grammar_studio/g8/student/epub/hmh_nglit_na_08_gse_m16/#cards--8le_03_gs_spelling_ml2/',
+        EDITABLE_GRAMMAR_PRACTICE: 'Prefixes and Suffixes',
+        EDITABLE_ICON_LINKS:
+            'https://www.hmhco.com/content/literature/into_lit/g8/student/doc/08le_03_pgp_prefixessuffixes.docx',
+        LEVEL_UP_TUTORIALS: 'Spelling Words Often Confused',
+        LEVEL_UP_TUTORIALS_LINKS:
+            'https://www.hmhco.com/content/literature/level_up/lit137/index.html',
+        LEVEL_UP_PRACTICE_TESTS: 'Spelling Words Often Confused',
+        LEVEL_UP_PRACTICE_TEST_ICONs: true,
+    },
+    {
+        LESSONS: 'Prefixes and Suffixes',
+        LESSONS_LINKS:
+            'https://www.hmhco.com/content/literature/grammar_studio/g8/student/epub/hmh_nglit_na_08_gse_m16/#cards--8le_03_gs_spelling_ml3/',
+        EDITABLE_GRAMMAR_PRACTICE: 'Plurals of Nouns',
+        EDITABLE_ICON_LINKS:
+            'https://www.hmhco.com/content/literature/into_lit/g8/student/doc/08le_03_pgp_pluralsnoun.docx',
+    },
+    {
+        LESSONS: 'Plurals of Nouns',
+        LESSONS_LINKS:
+            'https://www.hmhco.com/content/literature/grammar_studio/g8/student/epub/hmh_nglit_na_08_gse_m16/#cards--8le_03_gs_spelling_ml4/',
+        EDITABLE_GRAMMAR_PRACTICE: 'Spelling Words',
+        EDITABLE_ICON_LINKS:
+            'https://www.hmhco.com/content/literature/into_lit/g8/student/doc/08le_03_pgp_spellingwords.docx',
+    },
+    {
+        LESSONS: 'Spelling Words',
+        LESSONS_LINKS:
+            'https://www.hmhco.com/content/literature/grammar_studio/g8/student/epub/hmh_nglit_na_08_gse_m16/#cards--8le_03_gs_spelling_ml5/',
+        EDITABLE_GRAMMAR_PRACTICE: 'Spelling Module Review',
+        EDITABLE_ICON_LINKS:
+            'https://www.hmhco.com/content/literature/into_lit/g8/student/doc/08le_03_pgp_spellingmodrev.docx',
+    },
+    {
+        LESSONS: 'Silent Letters',
+        LESSONS_LINKS:
+            'https://www.hmhco.com/content/literature/grammar_studio/g8/student/epub/hmh_nglit_na_08_gse_m16/#cards--8le_03_gs_spelling_ml5/',
+        EDITABLE_GRAMMAR_PRACTICE: 'Editable Grammar Practice Answer Key',
+        EDITABLE_ICON_LINKS:
+            'https://www.hmhco.com/content/literature/into_lit/g8/teacher/pdf/08le_03_pgp_answerkey.pdf',
+    },
 ];
